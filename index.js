5398d396705a98ba84e5b3ca8904d66ec30626f2
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-27 07:14:23 -0300

    [ADDED] canvas dimensions

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ req = new XMLHttpRequest();
 let baseTemp;
 let values = [];
 
+let width = 1200;
+let height = 600;
+let padding = 600;
+
+/*the html element svg selector. Then variables defined above were used to give width and height attributes*/
+let canvas = d3.select('#canvas');
+let drawCanvas =()=> {
+    canvas.attr('width', width);
+    canvas.attr('height', height);
+};
+
 /*used the open method to set the XMLHttpRequest. The first argument is the 'GET' method
 as we are fetching info from an url (which is the second argument). The third argument
 is a boolean wheter we want to run the function as asyncronous or not.*/
 req.open('GET', url, true);
-/*what to do with the data when we get it. In this line of code it is formated from a JSON
-string into a javascript object.*/
+/*what to do with the data when we get it. First it is formated from a JSON string into a
+javascript object. Then we assing data values to the variables baseTemp and values*/
 req.onload =()=>{
     let data = JSON.parse(req.responseText);
     baseTemp = data['baseTemperature'];
-    values = object['monthlyVariance']
+    values = data['monthlyVariance'];
+    drawCanvas()
 }
+/*this method sends the request*/
+req.send();
